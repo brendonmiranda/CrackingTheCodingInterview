41239ddf47b049b239abbede2cf8bf6e86e1e5ae
brendonmiranda <36212954+brendonmiranda@users.noreply.github.com>
2022-05-07 17:28:14 +0100

    adding one more test for interview questions 5.1

--- src/test/java/cracking/the/code/interview/chapter5/Question51Test.java
+++ src/test/java/cracking/the/code/interview/chapter5/Question51Test.java
@@ -10,18 +10,24 @@ public class Question51Test {
     @Test
     public void test() {
 
-        long result = 0B10001001100;
+        final long result = 0B10001001100;
 
         long n = 0B10000000000L;
-        long m = 0B10011L;
-        int i = 2;
-        int j = 6;
+
+        final long m = 0B10011L;
+        final int i = 2;
+        final int j = 6;
 
         Assertions.assertEquals(toBinaryString(result), toBinaryString(Question51.insertMIntoN(n, m, i, j)));
 
-        n = 0B10000010000L; // the 5 bit needs to be cleaned
+        n = 0B10000010000L;
 
         Assertions.assertEquals(toBinaryString(result), toBinaryString(Question51.insertMIntoN(n, m, i, j)));
+
+        n = 0B10000110000L;
+
+        Assertions.assertEquals(toBinaryString(result), toBinaryString(Question51.insertMIntoN(n, m, i, j)));
+
     }
 
 }
