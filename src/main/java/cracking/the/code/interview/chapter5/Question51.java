18d2bbea2b4e5374dcbf438344f86097f5079178
brendonmiranda <36212954+brendonmiranda@users.noreply.github.com>
2022-05-07 17:12:18 +0100

    improving interview question 5.1

--- src/main/java/cracking/the/code/interview/chapter5/Question51.java
+++ src/main/java/cracking/the/code/interview/chapter5/Question51.java
@@ -7,9 +7,31 @@ package cracking.the.code.interview.chapter5;
  */
 public class Question51 {
 
+    private static final long BITS_32 = (long) Math.pow(2, 32) - 1;
+
     public static long insertMIntoN(Long n, Long m, int i, int j) {
 
-        return n | (m << i);
+        // 1. clean the bits of N between i and j, also i and j itself.
+        // 2. shift M to the left based on i
+        // 3. set M into the N (after clean)
+
+        // if j=6 then 111111111111111111111111111111110000000
+        long maskForRightZeros = (BITS_32 << (j + 1));
+
+        // 11111111111111111111111110000000
+        long leftOnesRightZeros32Bits = BITS_32 & maskForRightZeros;
+
+        // if i = 2 then 00000000000000000000000000000100 - 1 which is equals 00000000000000000000000000000011
+        long onesBeforeStartBits = (1L << i) - 1;
+
+        // 11111111111111111111111110000000 | 00000000000000000000000000000011 = 11111111111111111111111110000011
+        long maskToCleanBits = leftOnesRightZeros32Bits | onesBeforeStartBits;
+
+        // if n = 00000000000000000000010000010000 then 00000000000000000000010000010000 & 11111111111111111111111110000011 = 00000000000000000000010000000000
+        long nCleaned = n & maskToCleanBits;
+
+        // if m = 10011 and i = 2 then 00000000000000000000000001001100 | nCleaned ( which is 00000000000000000000010000000000)
+        return nCleaned | (m << i);
 
     }
 
--- src/test/java/cracking/the/code/interview/chapter5/Question51Test.java
+++ src/test/java/cracking/the/code/interview/chapter5/Question51Test.java
@@ -10,15 +10,18 @@ public class Question51Test {
     @Test
     public void test() {
 
+        long result = 0B10001001100;
+
         long n = 0B10000000000L;
         long m = 0B10011L;
         int i = 2;
         int j = 6;
 
-        long result = 0B10001001100;
-
         Assertions.assertEquals(toBinaryString(result), toBinaryString(Question51.insertMIntoN(n, m, i, j)));
 
+        n = 0B10000010000L; // the 5 bit needs to be cleaned
+
+        Assertions.assertEquals(toBinaryString(result), toBinaryString(Question51.insertMIntoN(n, m, i, j)));
     }
 
 }
