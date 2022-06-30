e4b16d0884424e6de9fc7cedbe91240ba6db7809
brendonmiranda <36212954+brendonmiranda@users.noreply.github.com>
2022-06-30 01:59:34 +0100

    describing my countingSort1 solution

--- src/main/java/hackerRank/week1/CountingSort1.java
+++ src/main/java/hackerRank/week1/CountingSort1.java
@@ -5,6 +5,11 @@ import java.util.List;
 
 /**
  * Problem statement: https://github.com/brendonmiranda/CrackingTheCodingInterview/blob/main/src/main/resources/one-month-preparation-kit-countingsort1-English.pdf
+ * I guess this is a O(n) solution. The arrayList add() operation will be o(1) because I'm adding at the last index, and I won't need to resize the array.
+ * Also, the set() operation will only replace the value in the index. No shift or resize is needed, so o(1).
+ *
+ * I would be able to avoid the first loop if I could return an array of primitive int instead of an arrayList of Integers.
+ * I'd be able to initialize an int array that has 0 as default value for each index. e.g. new int[] array = new int[100]
  */
 public class CountingSort1 {
 
@@ -13,12 +18,11 @@ public class CountingSort1 {
         final List<Integer> res = new ArrayList<>(100);
 
         for (int i = 0; i < 100; i++) {
-            res.add(i, 0);
+            res.add(0);
         }
 
-
         for (Integer v : arr) {
-            if (v <= 100) {
+            if (v < 100) {
                 res.set(v, res.get(v) + 1);
             }
         }
