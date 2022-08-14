af4247084458632faff44c8f51e6911906ce6354
brendonmiranda <36212954+brendonmiranda@users.noreply.github.com>
2022-08-14 22:57:31 +0100

    adding some comments for the drawing book solution

--- src/main/java/hackerRank/week2/DrawingBook.java
+++ src/main/java/hackerRank/week2/DrawingBook.java
@@ -9,10 +9,14 @@ public class DrawingBook {
 
         // [_ 1][2 3][4 5][6 7]
 
+        // checks how many pages I need to flip to go to the page p from the front of the book
         int front = p/2;
 
+        // checks how many pages I need to flip to go to the last page and subtract the value above
+        // this is going to give me how many pages I need to flip to go to page p from the back of the book
         int back = (n / 2) - front;
 
+        // return the minimum value
         return Math.min(front, back);
 
     }
