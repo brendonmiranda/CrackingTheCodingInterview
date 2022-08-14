ad74f29ab2bfdbd5d2076f6f5c426966c6655949
brendonmiranda <36212954+brendonmiranda@users.noreply.github.com>
2022-08-14 01:16:39 +0100

    hackerrank salesByMatch solution

---
+++ src/main/java/hackerRank/week2/SalesByMatch.java
@@ -0,0 +1,42 @@
+package hackerRank.week2;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+
+/**
+ * Problem statement:
+ * I guess this a linear solution O(n) where n = list size
+ */
+public class SalesByMatch {
+
+    public static int sockMerchant(int n, List<Integer> ar) {
+
+        int pairs = 0;
+
+        // create a map
+        Map<Integer, Integer> socksCountMap = new HashMap<>();
+
+        // for through the list
+        for (Integer i: ar) {
+            // add the number as the key
+            // add the value as a counter
+            Integer value = socksCountMap.getOrDefault(i, 0);
+            socksCountMap.put(i, ++value);
+        }
+
+        // for through the keys of the map
+        Set<Integer> keySet = socksCountMap.keySet();
+
+        for (Integer i: keySet) {
+            // retrieve the value from the key
+            Integer value = socksCountMap.get(i);
+            pairs += value/2;
+        }
+
+        return pairs;
+
+    }
+
+}
